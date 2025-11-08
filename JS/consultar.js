a9260b239189938910289a21f81913fbb6da6436
Mirko Becker <mirkobecker343@gmail.com>
2025-11-08 17:36:28 -0300

    Primer commit. Vistas index, consultar y crear funcionando.

---
+++ JS/consultar.js
@@ -0,0 +1,44 @@
+/* ============================= Varibales ============================= */
+let getProductsForm = document.getElementById("get-products-form");
+let listadoProductos = document.getElementById("listado-productos"); // Listado donde se van a mostrar los resultados de la búsqueda
+
+
+
+
+/* ============================= Funciones ============================= */
+getProductsForm.addEventListener("submit", async(event) =>{ // Obtener datos cargados por el usuario en el form.
+    event.preventDefault();
+
+    let formData = new FormData(event.target);
+    let data = Object.fromEntries(formData.entries());
+    let idProducto = data.id;
+
+    console.log("ID consultado: ", idProducto);
+
+    try{
+        let url = `http://localhost:3000/products/${idProducto}` // URL a la que voy a hacer el fetch para enviar la request
+
+        let response = await fetch(url); // Petición get a la url. fetch por defecto utiliza el método get.
+
+        let datos = await response.json();
+
+        let producto = datos.payload[0]; // La primera posición del array payload está ocupada por el array con los datos que necesito
+
+        mostrarProducto(producto);
+    }catch(error){
+        console.log("Error consultando producto por ID: ", error);
+    }
+});
+
+function mostrarProducto(producto){
+    console.table(producto);
+
+    let htmlProducto = `
+                    <li class="li-listados">
+                    <img src="${producto.IMG_URL}" alt="${producto.NOMBRE}" class="img-li-listados">
+                    <p>ID: ${producto.ID}/ Nombre: ${producto.NOMBRE} / <strong>Precio: $${producto.PRECIO}</strong></p>
+                    </li>
+                `
+
+    listadoProductos.innerHTML = htmlProducto;
+}
---
+++ JS/main.js
@@ -0,0 +1,68 @@
+/* ============================= Varibales ============================= */
+const url = "http://localhost:3000/products"
+
+let catalogo = document.querySelector("#catalogo");
+
+
+
+
+/* ============================= Funciones ============================= */
+// Consumir JSON para los productos.
+async function obtenerDatos(){ // Las funciones async siempre devuelven una promesa, independientemente del valor de lo que retorna.
+
+    try {
+        let respuesta = await fetch(url); // Hacemos una peticion a nuestro nuevo endpoint en http://localhost:3000/products
+
+        let data = await respuesta.json();
+
+        console.log(data); // Nuestros productos estan disponibles dentro de payload { payload: Array(19) }
+
+        let productos = data.payload;
+
+        mostrarProductos(productos);
+    } catch(error) {
+            console.error(error);
+    }
+}
+
+
+function mostrarProductos(arrayProductos){
+    
+    console.table(arrayProductos); // Recibimos correctamente en formato tabla los productos que nos manda la funcion obtenerProductos()
+    let htmlProducto = "";
+    arrayProductos.forEach(producto => {
+        htmlProducto += `
+            <div class="card-producto">
+                <img src="${producto.IMG_URL}" alt="${producto.NOMBRE}">
+                <h5>${producto.NOMBRE}</h5>
+                <p>Id: ${producto.ID}</p>
+                <p>$${producto.PRECIO}</p>
+            </div>
+        `;
+    });
+
+    catalogo.innerHTML = htmlProducto;
+}
+
+
+function init(){
+    obtenerDatos();
+}
+
+
+
+
+/* ============================= Llamadas ============================= */
+init();
+
+
+
+
+/* Ciclo de ejecución explicado 
+    1. Se llama a init() > esta función llama a la función cargarDatos() 
+    2. cargarDatos() En la línea 13 guarda en la variable dataArray el retorno de la función obtenerDatos(), se usa await porque tiene que esperar a que la función obtenerDatos() termine 
+    de ejecutar todo lo que tiene dentro y como es una función asíncrona no se hace en el momento, por eso se espera.
+        2.1. Dentro de la función obtenerDatos() se consume la información de la URL que le pasé a fetch. 
+        2.2. Después se transforma ese JSON en un array de objetos JavaScript. Se usa await porque tengo que esperar a que termine de consumirse lo que tiene la URL 
+    3. Una vez que finalizó la ejecución de obtenerDatos() y ya tengo el retorno (array de objetos JS) guardado en una variable, le paso dicho array a la función mostrarCatalogo()
+    4. mostrarCatalogo() carga el HTML con la información de los productos. */
