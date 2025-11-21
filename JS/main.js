b98c222ea71ecd83b1985cb8903ed3011809b711
Mirko Becker <mirkobecker343@gmail.com>
2025-11-21 14:51:53 -0300

    Se agregó funcionalidad al filtro por equipos. Se añadieron detalles a las tarjetas de producto

--- JS/main.js
+++ JS/main.js
@@ -4,11 +4,37 @@ const url = "http://localhost:3000/api/products";
 let catalogo = document.querySelector("#catalogo");
 let barraBusqueda = document.querySelector("#barra-busqueda");
 
+let ferrariIcon = document.querySelector("#icon-ferrari");
+let aplineIcon = document.querySelector("#icon-apline");
+let mercedesIcon = document.querySelector("#icon-mercedes");
+let rbIcon = document.querySelector("#icon-rb");
+let asmartinIcon = document.querySelector("#icon-asmartin");
+let haasIcon = document.querySelector("#icon-haas");
+let kickIcon = document.querySelector("#icon-kick");
+let williamsIcon = document.querySelector("#icon-williams");
+let rbcashIcon = document.querySelector("#icon-rbcash");
+
+let headerImg = document.querySelector("#header-img");
+
+
+
 barraBusqueda.addEventListener("keyup", filtrarProductos);
+
+ferrariIcon.addEventListener("click", () => filtrarProductosPorEquipo("Ferrari"));
+aplineIcon.addEventListener("click", () => filtrarProductosPorEquipo("Alpine"));
+mercedesIcon.addEventListener("click", () => filtrarProductosPorEquipo("Mercedes"));
+rbIcon.addEventListener("click", () => filtrarProductosPorEquipo("Red Bull"));
+asmartinIcon.addEventListener("click", () => filtrarProductosPorEquipo("Aston Martin"));
+haasIcon.addEventListener("click", () => filtrarProductosPorEquipo("Haas"));
+kickIcon.addEventListener("click", () => filtrarProductosPorEquipo("Sauber"));
+williamsIcon.addEventListener("click", () => filtrarProductosPorEquipo("Williams"));
+rbcashIcon.addEventListener("click", () => filtrarProductosPorEquipo("VCARB"));
+
+headerImg.addEventListener("click", obtenerDatos);
 
 /* ============================= Funciones ============================= */
 // Consumir JSON para los productos.
-async function obtenerDatos(filtrar){ // Las funciones async siempre devuelven una promesa, independientemente del valor de lo que retorna.
+async function obtenerDatos(filtrar, equipo){ // Las funciones async siempre devuelven una promesa, independientemente del valor de lo que retorna.
 
     try {
         let respuesta = await fetch(url); // Hacemos una peticion a nuestro nuevo endpoint en http://localhost:3000/products
@@ -17,8 +43,11 @@ async function obtenerDatos(filtrar){ // Las funciones async siempre devuelven u
 
         let productos = data.payload; // Nuestros productos estan disponibles dentro de payload { payload: Array(19) }
 
-        if(filtrar){
+        if(filtrar && equipo === null){
             let productosFiltrados = productos.filter((producto) => producto.NOMBRE.toLowerCase().includes(barraBusqueda.value.toLowerCase()));
+            return productosFiltrados;
+        }else if(filtrar && equipo){
+            let productosFiltrados = productos.filter((producto) => producto.NOMBRE.toLowerCase().includes(equipo.toLowerCase()));
             return productosFiltrados;
         }else{
             mostrarProductos(productos);
@@ -29,8 +58,14 @@ async function obtenerDatos(filtrar){ // Las funciones async siempre devuelven u
     }
 }
 
-async function filtrarProductos(){
-    let productosFiltrados = await obtenerDatos(true);
+async function filtrarProductosPorEquipo(equipo){
+    console.log(equipo);
+    let productosFiltrados = await obtenerDatos(true, equipo);
+    mostrarProductos(productosFiltrados, null);
+}
+
+async function filtrarProductos(){  
+    let productosFiltrados = await obtenerDatos(true, null);
     mostrarProductos(productosFiltrados);
 }
 
@@ -43,8 +78,13 @@ function mostrarProductos(arrayProductos){
             <div class="card-producto">
                 <img src="${producto.IMG_URL}" alt="${producto.NOMBRE}">
                 <h5>${producto.NOMBRE}</h5>
-                <p>Id: ${producto.ID}</p>
-                <p>$${producto.PRECIO}</p>
+                <div class = "product-data-container"> 
+                    <p>Id: ${producto.ID}</p>
+                    <p><strong>$${producto.PRECIO}</strong></p>
+                    <div class="cart-img-container">
+                        <img src="./IMG/cart_icon.jpg">
+                    </div>
+                </div>
             </div>
         `;
     });
